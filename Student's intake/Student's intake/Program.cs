0e1b11d18c2e854ddfd26571aaf49d38d244621d
Benny228 <124263912+Benny228@users.noreply.github.com>
2023-05-12 13:48:42 +0300

    Add files via upload

---
+++ Student's intake/Student's intake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_s_intake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A program 2 intake name,roll no. and field of interestof the User
+            string K;
+            string B;
+            string R;
+
+            Console.WriteLine("Enter your name");
+            K = Console.ReadLine();
+
+            Console.WriteLine("Enter your Roll number");
+            B = Console.ReadLine();
+
+            Console.WriteLine("Enter your Field of interest");
+            R = Console.ReadLine();
+            Console.WriteLine("HEY, MY NAME IS {0} AND MY ROLL NUMBER IS {1}.MY FIELDS OF INTEREST ARE {2}.", K, B, R);
+        }
+    }
+}
